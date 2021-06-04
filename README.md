59c41509110c1b784a7baf6b96ef49e2c756eb96
shantanhunt <shantanusontakke015@gmail.com>
2021-06-04 08:25:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NPM Packages
+- https://www.npmjs.com/package/openzeppelin-solidity/v/2.5.0
---
+++ contracts/Migrations.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import 'openzeppelin/contracts/Crowdsale/Crowdsale.sol';
+import 'openzeppelin/contracts/token/ERC20/IERC20.sol';
+import 'openzeppelin/contracts/Crowdsale/distribution/PostDeliveryCrowdsale.sol';
+
+
+
+// contract ICO is Crowdsale, PostDeliveryCrowdsale{
+//   constructor(
+//     uint rate,
+//     address payable wallet,
+//     IERC20 token
+//   ) Crowdsale (rate, wallet, token) 
+//   public{}
+// }
+
+
+// contract Migrations {
+//   address public owner = msg.sender;
+//   uint public last_completed_migration;
+
+//   modifier restricted() {
+//     require(
+//       msg.sender == owner,
+//       "This function is restricted to the contract's owner"
+//     );
+//     _;
+//   }
+
+//   function setCompleted(uint completed) public restricted {
+//     last_completed_migration = completed;
+//   }
+// }
