582a990f216cab92f04171fce9ecbd41b88f8b23
shantanhunt <shantanusontakke015@gmail.com>
2021-06-05 06:32:39 -0700

    Test passed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NPM Packages
 - https://www.npmjs.com/package/openzeppelin-solidity/v/2.5.0
+
+# Notes
+- ERC20Standard from Openzeppelin is ERC20 now
+- (Using Openzeppelin V2.5)[https://github.com/OpenZeppelin/openzeppelin-contracts/tree/release-v2.5.0/contracts/token/ERC20]
--- test/DappTokenTest.js
+++ test/DappTokenTest.js
@@ -1,14 +1,16 @@
-cosnt DappToken = artifacts.require("DappToken");
+const DappToken = artifacts.require("DappToken");
 
 contract('Dapptoken', accounts=> {
+
+	beforeEach(async () => {
+		this.token = await DappToken.new('Dapp Token', 'DTC', 18);
+	});
+
 	describe('Token attributes', () =>{
-		it('Has the correct name', () => {
-			Dapptoken.deployed().then((instance) => {
-				deployedToken = instance;
-				instance.name().then((n) => {
-					assert.equal(n, 'Dapp Token');
-				})
-			})
+		it('Has the correct name', async () => {
+			const name = await this.token.name();
+			assert.equal(name, "Dapp Token")
 		})
-	})
+	
 });
+})
