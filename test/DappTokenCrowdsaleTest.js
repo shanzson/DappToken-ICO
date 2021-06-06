30a065c30aeaf518939df7f85e3590aa3aefe728
shantanhunt <shantanusontakke015@gmail.com>
2021-06-06 00:45:14 -0700

    Minted Crowdsale working

--- test/DappTokenCrowdsaleTest.js
+++ test/DappTokenCrowdsaleTest.js
@@ -1,17 +1,15 @@
-// const toWei = (x) => {
-//   return (x * 10 ** 18).toString();
-// }
 const toWei = require('./helpers/toWei');
 const chai = require('chai');
-var should = require('chai').should();
 const BN = require('bn.js');
 
+require('chai')
+	.use(require('chai-bn')(BN))
+	.use(require('chai-as-promised'))
+	.should();
 
 const DappToken = artifacts.require("DappToken");
 const DappTokenCrowdsale = artifacts.require("DappTokenCrowdsale.sol");
 
-// Enable and inject BN dependency
-chai.use(require('chai-bn')(BN));
 
 contract('Dapptoken Crowdsale', ([_, wallet, investor1, investor2])=> {
 
@@ -43,12 +41,32 @@ contract('Dapptoken Crowdsale', ([_, wallet, investor1, investor2])=> {
 		});
 	});
 
+	describe('Minted Crowdsale', () =>{
+		it('Mints tokens after purchase', async () => {
+			const originalTotalSupply = await this.token.totalSupply();
+				await this.crowdsale.sendTransaction({ 	 
+				value: toWei(1), 
+				from: investor1 
+			}).should.be.fulfilled;
+			const newTotalSupply = await this.token.totalSupply();
+			assert.isTrue(newTotalSupply > originalTotalSupply);
+		});
+	});
+
+
 	describe('Accepting payments', () =>{
 		it('Should Accept payments', async () => {
 			const value = toWei(1);
-			await this.crowdsale.sendTransaction({value: value, from: investor1 });
+			const purchaser = investor2;
+			await this.crowdsale.sendTransaction({ 	 //calls function external payable in crowdsale.sol
+				value: value, 
+				from: investor1 
+			}).should.be.fulfilled;
 			//Previously got VM Exception here as crowdsale was not the minter, _ was.
 			//So added crowdsale contract address using MinterRole.sol from _ in BeforeEach.
+
+			await this.crowdsale.buyTokens(investor1, {value: value, from: purchaser}).should.be.fulfilled;
+		
 		});
 	});
 });
