e74384703c70f9f199a9e7fe4c2ad7b91dadd08f
shantanhunt <shantanusontakke015@gmail.com>
2021-06-08 11:25:44 -0700

    Solved 1 error

--- contracts/DappTokenCrowdsale.sol
+++ contracts/DappTokenCrowdsale.sol
@@ -3,31 +3,35 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol";
 
 
-contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale{
+contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale{
 	// Minimum investor contribution - 0.002 Ether
 	// Maximum investor contribution - 50 Ether
 	uint256 public investorMincap = 2000000000000000; //0.002 ether, here 2*10^15 wei
 	uint256 public investorHardcap = 50000000000000000000; //50 ether, here 50*10^18
 	mapping(address => uint256) public contributions;
 
-
 	constructor(
 		uint256 rate, 
 		address payable wallet, 
 		IERC20 token, 
-		uint256 cap) 
+		uint256 cap,
+		uint256 openingTime,
+		uint256 closingTime
+		) 
 	Crowdsale(rate, wallet, token)
 	CappedCrowdsale(cap)
+	TimedCrowdsale(openingTime, closingTime)
 	public {
 
 	}
 
-	function getUserContribution(address _beneficiary)
-    	public view returns (uint256){
+	function getUserContribution(address _beneficiary) 
+		public view returns (uint256){
     		return contributions[_beneficiary];
-  		}
+  	}
 
 	function _preValidatePurchase(
 		address beneficiary,
--- test/DappTokenCrowdsaleTest.js
+++ test/DappTokenCrowdsaleTest.js
@@ -2,6 +2,56 @@ const toWei = require('./helpers/toWei');
 const chai = require('chai');
 const BN = require('bn.js');
 
+// const { increaseTime, increaseTimeTo, duration } = require('./helpers/increaseTime');
+// const latestTime = require('./helpers/latestTime');
+
+Web3 = require('web3');
+let web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+web3.eth.getBlockNumber()
+	.then((block) => console.log('Done!', block));
+
+const BigNumber = web3.BigNumber;
+
+function increaseTime (duration) {
+  const id = Date.now();
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration],
+      id: id,
+    }, err1 => {
+      if (err1) return reject(err1);
+
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: id + 1,
+      }, (err2, res) => {
+        return err2 ? reject(err2) : resolve(res);
+      });
+    });
+  });
+}
+
+function increaseTimeTo (target) {
+  let now = latestTime();
+  if (target < now) throw Error(`Cannot increase current time(${now}) to a moment in the past(${target})`);
+  let diff = target - now;
+  return increaseTime(diff);
+}
+
+async function latestTime (a) {
+	// var blockNumber = web3.eth.getBlockNumber();
+    const t = await web3.eth.getBlock(a).then((block) => {
+    	const timestamp = block.timestamp;
+    	console.log(timestamp, ' with type', typeof(timestamp));
+    	return timestamp;
+    });
+    return t;
+}
+
 require('chai')
 	.use(require('chai-bn')(BN))
 	.use(require('chai-as-promised'))
@@ -16,10 +66,36 @@ contract('Dapptoken Crowdsale', ([_, wallet, investor1, investor2])=> {
 	beforeEach(async () => {
 		this.token = await DappToken.new('Dapp Token', 'DTC', 18);
 	
+		//duration
+		// let duration = {
+		//   seconds: function (val) { return val; },
+		//   minutes: function (val) { return val * this.seconds(60); },
+		//   hours: function (val) { return val * this.minutes(60); },
+		//   days: function (val) { return val * this.hours(24); },
+		//   weeks: function (val) { return val * this.days(7); },
+		//   years: function (val) { return val * this.days(365); },
+		// };
+
+		function weeks (val) { return val * 7 * 24 * 60 * 60; }
+
 		//Crowdsale config
 		this.rate = 500;       //rate is the conversion between wei and the smallest and indivisible token unit
 		this.wallet = wallet;  // Address where funds are collected
 		this.cap = toWei(100); //Total amount to be raised (100 Ether);
+
+		console.log('Weeks(1): ', weeks(1), typeof(weeks(1)));
+		var w = weeks(1);
+		console.log('w ', w);
+		var latest_time = await latestTime('latest');
+		console.log('latest_time ', latest_time);
+		const n = 1623175390 + 604800;
+		console.log('n ', n);
+		var oT = w + latest_time;
+
+		console.log('oT: ',oT);
+		const cT = oT + weeks(1);
+		// console.log('Opening Time ',(this.openingTime).toString());
+		// console.log('Closing Time ',(this.closingTime).toString());
 
 		this.investorMinCap = toWei(0.002);
 		this.investorHardCap = toWei(50);
@@ -28,11 +104,17 @@ contract('Dapptoken Crowdsale', ([_, wallet, investor1, investor2])=> {
 			this.rate, 
 			this.wallet, 
 			this.token.address,
-			this.cap
+			this.cap,
+			oT,
+			cT			
 		);
 
 		//Transfer token ownership to crowdsale
 		await this.token.addMinter(this.crowdsale.address);
+		
+		//Advance time to crowdsale start
+		await increaseTimeTo(this.openingTime + 1);
+
 	});
 
 	describe('Crowdsale', () =>{
@@ -56,6 +138,13 @@ contract('Dapptoken Crowdsale', ([_, wallet, investor1, investor2])=> {
 			cap.should.be.bignumber.equal(this.cap);
 		});
 	});
+
+	// describe('timed crowdsale', () => {
+ //    	it('is open', async () => {
+ //      		const isClosed = await this.crowdsale.hasClosed();
+ //      	isClosed.should.be.false;
+ //    	});
+ //  	});
 
 
 	describe('Minted Crowdsale', () =>{
